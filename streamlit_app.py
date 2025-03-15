5a73212389e3d1132ffdce04f3f7600e84a7fff3
Filo1102 <f.toniolo11@gmail.com>
2025-03-15 12:40:19 +0000

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,29 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title(":blue[USER SATISFACTION FORM]")
+st.write(":green[**Let us know your opinion. Your feedback is important for us**]")
+
+# Creating a form
+with st.form("user_form"):
+    # Text input (name)
+    name = st.text_input("Full Name:", placeholder="Enter your name...")
+
+    # Number input (age)
+    age = st.number_input("Age:", min_value=1, max_value=120, value=25, step=1)
+
+    # Select box (dropdown) for gender
+    gender = st.selectbox("Gender:", ["Select an option", "Male", "Female", "Other"])
+
+    # Slider for rating
+    Rating = st.slider("App's rating", 1, 5, 3)
+
+    Feedback = st.text_area("Any additional feedback or thoughts?", placeholder="Write your comment here...")
+    # Submit button
+    submitted = st.form_submit_button("Submit")
+
+# Display output after submission
+if submitted:
+    if name and gender != "Select an option":
+        st.success(f"✅ Form submitted!\n\n**Name:** {name}\n**Age:** {age}\n**Gender:** {gender}")
+    else:
+        st.warning("⚠️ Please fill out all fields correctly.")
